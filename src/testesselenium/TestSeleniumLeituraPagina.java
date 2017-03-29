95f600eafa3a10275c37f5aad4425c200348590c
PROFESSOR <PROFESSOR@C-LAB05-PROF>
2017-03-28 21:29:57 -0300

    Segunso Commit

--- src/testesselenium/TestSeleniumLeituraPagina.java
+++ src/testesselenium/TestSeleniumLeituraPagina.java
@@ -5,5 +5,5 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 public class TestSeleniumLeituraPagina {
-         
+  // comentario       
 }
